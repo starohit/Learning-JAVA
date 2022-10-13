4caa0ddd32a6ab5259561e06e35151111b9634e7
Rohit Singh <77211013+starohit@users.noreply.github.com>
2022-10-14 02:19:12 +0530

    constructor

---
+++ oops/constructors/Bike1.java
@@ -0,0 +1,11 @@
+package constructors;
+
+public class Bike1 {
+    //creating a default constructor
+    Bike1(){System.out.println("Bike is created");}
+    //main method
+    public static void main(String[] args) {
+//calling a default constructor
+        Bike1 b = new Bike1();
+    }
+}
