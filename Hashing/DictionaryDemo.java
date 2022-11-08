c0759d01876a9b682201aac44cd1ca9997c989a6
Rohit Singh <77211013+starohit@users.noreply.github.com>
2022-11-09 00:41:36 +0530

    Update DictionaryDemo.java

--- Hashing/DictionaryDemo.java
+++ Hashing/DictionaryDemo.java
@@ -1,3 +1,26 @@
-public class DictionaryDemo {
+package Hashing;
 
+import java.util.Hashtable;
+import java.util.Dictionary;
+
+public class DictionaryDemo {
+    public static void main(String[] args) {
+        Dictionary<String,String> phoneBook = new Hashtable<>();
+
+        phoneBook.put("Rohit", "2178");
+        phoneBook.put("Aarush", "1298");
+        phoneBook.put("Raj", "1877");
+        phoneBook.put("Aparna", "2001");
+        phoneBook.put("Raj","3452"); //updates Raj number
+
+        System.out.println(phoneBook);
+        System.out.println(phoneBook.get("Raj"));
+        System.out.println(phoneBook.get("axle"));
+
+        System.out.println(((Hashtable)phoneBook).values());
+        System.out.println(phoneBook.size());
+
+        phoneBook.remove("Aparna");
+        System.out.println(phoneBook);
+    }
 }
