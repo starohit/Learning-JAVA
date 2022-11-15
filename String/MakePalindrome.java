cb074a9dcf354400361a58d266d65a4f9dd9e506
Rohit Singh <77211013+starohit@users.noreply.github.com>
2022-11-16 01:44:18 +0530

    Add files via upload

--- String/MakePalindrome.java
+++ String/MakePalindrome.java
@@ -2,29 +2,38 @@ package String;
 
 public class MakePalindrome {
     public static void main(String[] args) {
-        String str = "Abcb";
+        String str = "bcba";
         System.out.println(makePalindrome(str));
     }
     public static boolean checkPalindrome(String str){
         int i = 0, j = str.length() - 1;
 
         while (i < j) {
-
             if (str.charAt(i) != str.charAt(j))
                 return false;
             i++;
             j--;
         }
 
-        // Given string is a palindrome
         return true;
     }
     public static String makePalindrome(String base){
         String pref = "";
+        String suff = "";
+        int j = 0;
         int i = base.length() - 1;
         while (!checkPalindrome(pref + base)){
             pref = pref + base.charAt(i);
             i--;
+        }
+        //return pref + base;
+        while (!checkPalindrome(base + suff)){
+            suff = base.charAt(j) + suff;
+            j++;
+        }
+        //return base + suff;
+        if(suff.length()<pref.length()){
+            return base + suff;
         }
         return pref + base;
     }
